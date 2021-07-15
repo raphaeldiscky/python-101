453050ddc92025070cf4f3eed13288815532329a
Raphael Discky <zundria.putra@gmail.com>
2021-07-15 13:25:16 +0700

    Add Node Before/After (doubly linked list)

--- 4 - Doubly Linked List/Doubly Linked List.py
+++ 4 - Doubly Linked List/Doubly Linked List.py
@@ -8,6 +8,13 @@
         a. append
         b. prepend
     
+    2) Add Node Before/After a specified node
+        a. Add Node After, two cases:
+            - the node that we have to insert after the new node is the last node so that the next of that node is null
+            - the node that we have to insert after the new node is not the last node 
+        b. Add Node Before, two cases:
+            - insert a new before the head node
+            - insert a new node before a node that is not a head node
 '''
 
 
@@ -48,15 +55,52 @@ class DoublyLinkedList:
             new_node = Node(data)
             self.head.prev = new_node
             new_node.next = self.head
+            new_node.prev = None
             self.head = new_node
+
+    def add_after_node(self, key, data):
+        cur = self.head
+        while cur:
+            if cur.next is None and cur.data == key:
+                self.append(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                nxt = cur.next
+                cur.next = new_node
+                new_node.next = nxt
+                new_node.prev = cur
+                nxt.prev = new_node
+                return
+            cur = cur.next
+
+    def add_before_node(self, key, data):
+        cur = self.head
+        while cur:
+            if cur.prev is None and cur.data == key:
+                self.prepend(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                prev = cur.prev
+                prev.next = new_node
+                cur.prev = new_node
+                new_node.next = cur
+                new_node.prev = prev
+                return
+            cur = cur.next
 
 
 dllist = DoublyLinkedList()
+
 dllist.prepend(0)
 dllist.append(1)
 dllist.append(2)
 dllist.append(3)
 dllist.append(4)
 dllist.prepend(5)
+
+dllist.add_after_node(3, 6)
+dllist.add_before_node(4, 9)
 
 dllist.print_list()

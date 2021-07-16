2ebe7635c6ec74c8686269c8684b2621cd5d9d6c
Raphael Discky <zundria.putra@gmail.com>
2021-07-16 20:26:29 +0700

    Level-Order Traversal (btree)

--- 6 - Binary Trees/Binary Trees.py
+++ 6 - Binary Trees/Binary Trees.py
@@ -15,7 +15,17 @@
             - traverse the left subtree by recursively calling the pre-order method
             - traverse the right subtree by recursively calling the pre-order method
             - display the data part of the root (or current node)
+    
+    2) Level Order Traversal
+        require a queue (last element = front, first element = back)
+        - enqueue root node
+        - dequeue from the queue and add it to traversal
+        - enqueue the children of the node we dequeue
+        - dequeue from the queue and add it to traversal, enqueue its children again
+        - repeat until queue == 0
 '''
+
+from Queue import Queue
 
 
 class Node(object):
@@ -36,6 +46,8 @@ class BinaryTree(object):
             return self.inorder_print(tree.root, "")
         elif traversal_type == "postorder":
             return self.postorder_print(tree.root, "")
+        elif traversal_type == "levelorder":
+            return self.levelorder_print(tree.root)
         else:
             print("Traversal type " + str(traversal_type) + " is not supported.")
             return False
@@ -64,6 +76,23 @@ class BinaryTree(object):
             traversal += (str(start.value) + '-')
         return traversal
 
+    def levelorder_print(self, start):
+        if start is None:
+            return
+
+        queue = Queue()
+        queue.enqueue(start)
+
+        traversal = ""
+        while len(queue) > 0:
+            traversal += str(queue.peek()) + "-"
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+        return traversal
 
 #               1
 #           /       \
@@ -71,14 +100,15 @@ class BinaryTree(object):
 #         /  \      /   \
 #        4    5     6   7
 
+
 tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
-tree.root.right.left = Node(6)
-tree.root.right.right = Node(7)
 
-print(tree.print_tree("preorder"))
-print(tree.print_tree("inorder"))
-print(tree.print_tree("postorder"))
+print(tree.print_tree("levelorder"))
+
+# print(tree.print_tree("preorder"))
+# print(tree.print_tree("inorder"))
+# print(tree.print_tree("postorder"))
---
+++ 6 - Binary Trees/Queue.py
@@ -0,0 +1,30 @@
+class Queue(object):
+    '''
+    last element = front, first element = back
+               +---+---+---+
+    enqueue -> | 3 | 2 | 1 | -> dequeue
+               +---+---+---+
+    '''
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)  # insert at start index
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)

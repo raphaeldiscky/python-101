122aaeda77ec301ad61cc6f09c3890d43b01dafc
Raphael Discky <zundria.putra@gmail.com>
2021-07-19 16:22:57 +0700

    Check Permutation (string)

---
+++ 10 - String Processing/6 - Check Permutation.py
@@ -0,0 +1,69 @@
+'''
+    1) Check Permutation
+        a. Solution 1
+        b. Solution 2
+
+    strings that are permutations of each other:
+        - is_permutation_1 = "google"
+        - is_permutation_2 = "ooggle"
+'''
+
+
+def is_perm_1(str_1, str_2):
+    # Approach 1: Sorting
+    # Time Complexity: O(n log n)
+    # Space Complexity: O(1)
+    str_1 = str_1.lower()
+    str_2 = str_2.lower()
+
+    if len(str_1) != len(str_2):
+        return False
+
+    # sorted -> return a list, join -> turned into a string
+    str_1 = ''.join(sorted(str_1))
+    str_2 = ''.join(sorted(str_2))
+
+    n = len(str_1)
+
+    for i in range(n):
+        if str_1[i] != str_2[i]:
+            return False
+    return True
+
+
+def is_perm_2(str_1, str_2):
+    # Approach 2: Hash Table
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    str_1 = str_1.lower()
+    str_2 = str_2.lower()
+
+    if len(str_1) != len(str_2):
+        return False
+
+    d = {}
+
+    for i in str_1:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    for i in str_2:
+        if i in d:
+            d[i] -= 1
+        else:
+            return False
+
+    return all(value == 0 for value in d.values())
+
+
+is_permutation_1 = "google"
+is_permutation_2 = "ooggle"
+
+not_permutation_1 = "not"
+not_permutation_2 = "top"
+print(is_perm_1(is_permutation_1, is_permutation_2))
+print(is_perm_1(not_permutation_1, not_permutation_2))
+
+print(is_perm_2(is_permutation_1, is_permutation_2))
+print(is_perm_2(not_permutation_1, not_permutation_2))
